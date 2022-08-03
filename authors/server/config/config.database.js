7f4060356d62668d41d85c2a2e60ea965441cdcd
rubenocasio <108745971+rubenocasio@users.noreply.github.com>
2022-08-03 12:08:52 -0400

    Update config.database.js

--- authors/server/config/config.database.js
+++ authors/server/config/config.database.js
@@ -5,5 +5,5 @@ mongoose.connect(`mongodb://localhost/${database}`, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
 })
-	.then(res => console.log("Established a connection to the Death Star"))
+	.then(res => console.log(`Established a connection to the Death Star - ${database}`))
 	.catch(err => console.log("Something went wrong when connecting to the Death Star", err));
