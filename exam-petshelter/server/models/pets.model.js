697bf673851cbf3d46295cf62f37985b54a89681
rubenocasio <108745971+rubenocasio@users.noreply.github.com>
2022-08-03 12:57:19 -0400

    Done!

--- exam-petshelter/server/models/pets.model.js
+++ exam-petshelter/server/models/pets.model.js
@@ -15,30 +15,31 @@ const PetSchema = new mongoose.Schema({
     },
     description:{
         type: String,
-        required:[true, "Hey you need to give a description of your pet idiot."],
+        required:[true, "Hey you need to give a description of your pet fool."],
         minlength:[3, "Need more input."],
         maxlength:[50, "Too long did not read."],
     },
     skill1: {
         type: String,
-        required:[true, "Hey you need to give a description of your pet idiot."],
+        required:[true, "Hey you need to give a description of your pet fool."],
         minlength:[3, "Need more input."],
         maxlength:[50, "Too long did not read."],
     },
     skill2: {
         type: String,
-        required:[true, "Hey you need to give a description of your pet idiot."],
+        required:[true, "Hey you need to give a description of your pet fool."],
         minlength:[3, "Need more input."],
         maxlength:[50, "Too long did not read."],
     },
     skill3: {
         type: String,
-        required:[true, "Hey you need to give a description of your pet idiot."],
+        required:[true, "Hey you need to give a description of your pet fool."],
         minlength:[3, "Need more input."],
         maxlength:[50, "Too long did not read."],
     }
 }, {timestamps:true})
 
 const Pet = mongoose.model("Pet", PetSchema);
+
 
 module.exports = Pet;
