ccbfe53fbbe2b922ac79c5f4a81d1ec3a9c3c1df
Ruben Ocasio <rocasio@rubenocasio.com>
2023-11-09 19:47:37 -0500

    Update jokes.routes.js

--- jokesAPI/server/routes/jokes.routes.js
+++ jokesAPI/server/routes/jokes.routes.js
@@ -2,6 +2,7 @@ const JokesController = require("../controllers/jokes.controllers");
 
 module.exports = (app) => {
     app.get("/api/jokes/", JokesController.findAll);
+    app.get("/api/jokes/rando", JokesController.randoJoke);
     app.get("/api/jokes/:id", JokesController.findOne);
     app.put("/api/jokes/update/:id", JokesController.updateJokes);
     app.post("/api/jokes/new", JokesController.createJokes);
